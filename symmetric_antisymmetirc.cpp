bd93f4107e9d92c7a910d3a54e2c93eae2bd1c4f
Subhkaran Singh <69869942+barbariansubhkaran@users.noreply.github.com>
2022-06-05 13:22:34 +0300

    Update symmetric_antisymmetirc.cpp

--- symmetric_antisymmetirc.cpp
+++ symmetric_antisymmetirc.cpp
@@ -3,11 +3,46 @@
 int main()
 {
     
-    Matrix3d T; 
+   int a11, a12, a13;
+   int a21, a22, a23;
+   int a31, a32, a33;
 
-    T << 1, 2, 3,
-         4, 2, 1,
-         1, 1, 1;
+   cout << "Enter elements of the matirix =\n" << endl;
+
+   cout << "Enter value of a11 : ";
+   cin >> a11;
+
+   cout << "Enter value of a12 : ";
+   cin >> a12;
+
+   cout << "Enter value of a13 : ";
+   cin >> a13;
+
+   cout << "Enter value of a21 : ";
+   cin >> a21;
+
+   cout << "Enter value of a22 : ";
+   cin >> a22;
+
+   cout << "Enter value of a23 : ";
+   cin >> a23;
+
+   cout << "Enter value of a31 : ";
+   cin >> a31;
+
+   cout << "Enter value of a32 : ";
+   cin >> a32;
+
+   cout << "Enter value of a33 : ";
+   cin >> a33;
+
+
+
+    Matrix3d T;
+
+    T << a11, a12, a13,
+         a21, a22, a23,
+         a31, a32, a33;
 
   cout << "Tensor =\n" << T << endl;
 
